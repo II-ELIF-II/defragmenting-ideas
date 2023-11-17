0900a947e3d8b7290ee0235f4bca16d7ef52cb4f
II-ELIF-II <61645475+II-ELIF-II@users.noreply.github.com>
2023-11-18 02:36:22 +0800

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return <h1>Rick and Morty</h1>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
+
+
+
+
+require('dotenv').config()
+
+const mysql = require('mysql2')
+
+// Create the connection to the database
+const connection = mysql.createConnection(process.env.REACT_APP_API_KEY)
+
+// simple query
+connection.query('show tables', function (err, results, fields) {
+  console.log(results) // results contains rows returned by server
+  console.log(fields) // fields contains extra metadata about results, if available
+})
+
+// Example with placeholders
+connection.query('select 1 from dual where ? = ?', [1, 1], function (err, results) {
+  console.log(results)
+})
+
+connection.end()
