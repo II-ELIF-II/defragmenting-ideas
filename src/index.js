51d9849c3c552e5c96ea3c9f9cd2ecbf64b75275
II-ELIF-II <61645475+II-ELIF-II@users.noreply.github.com>
2023-11-18 03:21:31 +0800

    Initial

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App'
 
-function App() {
-  return <h1>Rick and Morty</h1>;
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+// const mysql = require('mysql2')
 
-//const mysql = require('mysql2')
+// // Create the connection to the database
+// const connection = mysql.createConnection(process.env.REACT_APP_API_KEY)
 
-// Create the connection to the database
-const connection = mysql.createConnection(process.env.REACT_APP_API_KEY)
+// // simple query
+// connection.query('show tables', function (err, results, fields) {
+//   console.log(results) // results contains rows returned by server
+//   console.log(fields) // fields contains extra metadata about results, if available
+// })
 
-// simple query
-connection.query('show tables', function (err, results, fields) {
-  console.log(results) // results contains rows returned by server
-  console.log(fields) // fields contains extra metadata about results, if available
-})
+// // Example with placeholders
+// connection.query('select 1 from dual where ? = ?', [1, 1], function (err, results) {
+//   console.log(results)
+// })
 
-// Example with placeholders
-connection.query('select 1 from dual where ? = ?', [1, 1], function (err, results) {
-  console.log(results)
-})
-
-connection.end()
+// connection.end()
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      //filename: './index.html',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         },
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-  ],
   
 };
